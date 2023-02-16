3b129cd04afcc34a944d80ffd09eb7e127de4754
captainBeckett <113695934+captainBeckett@users.noreply.github.com>
2023-02-16 16:41:09 -0500

    Add main file

---
+++ Main.java
@@ -0,0 +1,15 @@
+/**
+ * Represents a file to run Tank Madness game
+ * 
+ * @author captainBeckett
+ */
+public class Main {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // creates an instance of my game
+        Interface game = new Interface();
+    }
+}
